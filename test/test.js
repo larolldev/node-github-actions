b5f73b34b973f13231ba8e7f771a16f6b9aece46
larolldev <larolldev@gmail.com>
2021-05-05 16:24:09 +1000

    pusher

--- test/test.js
+++ test/test.js
@@ -1,10 +1,13 @@
-const request = require('supertest')
-const app = require('../index')
+const Pusher = require('pusher')
 
-describe('GET /', () => {
-  it('respond with Hello World', done => {
-    request(app)
-      .get('/')
-      .expect('Hello World', done)
-  })
+const pusher = new Pusher({
+  appId: '1194179',
+  key: 'd426de2ce4b4a63e74b6',
+  secret: '110814cc70f09c03b206',
+  cluster: 'ap1',
+  useTLS: true
+})
+
+pusher.trigger('my-channel', 'my-event', {
+  message: 'hello world'
 })
